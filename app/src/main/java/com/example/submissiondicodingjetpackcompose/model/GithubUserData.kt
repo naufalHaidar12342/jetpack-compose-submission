d3fe6f034d63bf00c67a22746b7b12626c9961c8
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-15 11:45:17 +0700

    add avatar. assets from submission1 fundamental android

---
+++ app/src/main/java/com/example/submissiondicodingjetpackcompose/model/GithubUserData.kt
@@ -0,0 +1,76 @@
+package com.example.submissiondicodingjetpackcompose.model
+
+object GithubUserData {
+    val githubUsers= listOf<GithubUser>(
+        GithubUser(
+            username = "JakeWharton",
+            fullName = "Jake Wharton",
+            profilePicture = 1,
+            company = "Google, Inc.",
+            location = "Pittsburgh, PA, USA"
+        ),
+        GithubUser(
+            username = "amitshekhariitbhu",
+            fullName = "Amit Shekhar",
+            profilePicture = 1,
+            company = "@MindOrksOpenSource",
+            location = "New Delhi, India"
+        ),
+        GithubUser(
+            username = "JakeWharton",
+            fullName = "Jake Wharton",
+            profilePicture = 1,
+            company = "Google, Inc.",
+            location = "Pittsburgh, PA, USA"
+        ),
+        GithubUser(
+            username = "romainguy",
+            fullName = "Romain Guy",
+            profilePicture = 1,
+            company = "Google, Inc.",
+            location = "California"
+        ),
+        GithubUser(
+            username = "chrisbanes",
+            fullName = "Chris Banes",
+            profilePicture = 1,
+            company = "@google working on @android",
+            location = "Sydney, Australia"
+        ),
+        GithubUser(
+            username = "tipsy",
+            fullName = "David",
+            profilePicture = 1,
+            company = "Working Group Two",
+            location = "Trondheim, Norway"
+        ),
+        GithubUser(
+            username = "ravi8x",
+            fullName = "Ravi Tamada",
+            profilePicture = 1,
+            company = "AndroidHive | Droid5",
+            location = "India"
+        ),
+        GithubUser(
+            username = "jasoet",
+            fullName = "Deny Prasetyo",
+            profilePicture = 1,
+            company = "@gojek-engineering",
+            location = "Kotagede, Yogyakarta, Indonesia"
+        ),
+        GithubUser(
+            username = "budioktaviyan",
+            fullName = "Budi Oktaviyan",
+            profilePicture = 1,
+            company = "@KotlinID",
+            location = "Jakarta, Indonesia"
+        ),GithubUser(
+            username = "hendisantika",
+            fullName = "Hendi Santika",
+            profilePicture = 1,
+            company = "@JVMDeveloperID @KotlinID @IDDevOps",
+            location = "Bojongsoang - Bandung Jawa Barat"
+        ),
+
+    )
+}
