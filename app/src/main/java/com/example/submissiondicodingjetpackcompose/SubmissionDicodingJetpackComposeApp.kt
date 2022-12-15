70210d4a9837b8bf422c57ca9d395a9903c37e88
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-12-15 12:23:58 +0700

    replace profilePicture value for each list member, add modifier as parameter for DicodingComposeApp

--- app/src/main/java/com/example/submissiondicodingjetpackcompose/SubmissionDicodingJetpackComposeApp.kt
+++ app/src/main/java/com/example/submissiondicodingjetpackcompose/SubmissionDicodingJetpackComposeApp.kt
@@ -2,11 +2,12 @@ package com.example.submissiondicodingjetpackcompose
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.submissiondicodingjetpackcompose.ui.theme.SubmissionDicodingJetpackComposeTheme
 
 @Composable
-fun SubmissionDicodingComposeApp(){
+fun SubmissionDicodingComposeApp(modifier: Modifier=Modifier){
 
 }
 
--- app/src/main/java/com/example/submissiondicodingjetpackcompose/model/GithubUserData.kt
+++ app/src/main/java/com/example/submissiondicodingjetpackcompose/model/GithubUserData.kt
@@ -1,75 +1,78 @@
 package com.example.submissiondicodingjetpackcompose.model
 
+import com.example.submissiondicodingjetpackcompose.R
+
 object GithubUserData {
-    val githubUsers= listOf<GithubUser>(
+    val githubUsers= listOf(
         GithubUser(
             username = "JakeWharton",
             fullName = "Jake Wharton",
-            profilePicture = 1,
+            profilePicture = R.drawable.user1,
             company = "Google, Inc.",
             location = "Pittsburgh, PA, USA"
         ),
         GithubUser(
             username = "amitshekhariitbhu",
             fullName = "Amit Shekhar",
-            profilePicture = 1,
+            profilePicture = R.drawable.user2,
             company = "@MindOrksOpenSource",
             location = "New Delhi, India"
         ),
         GithubUser(
-            username = "JakeWharton",
-            fullName = "Jake Wharton",
-            profilePicture = 1,
-            company = "Google, Inc.",
-            location = "Pittsburgh, PA, USA"
-        ),
-        GithubUser(
             username = "romainguy",
             fullName = "Romain Guy",
-            profilePicture = 1,
+            profilePicture = R.drawable.user3,
             company = "Google, Inc.",
             location = "California"
         ),
         GithubUser(
             username = "chrisbanes",
             fullName = "Chris Banes",
-            profilePicture = 1,
+            profilePicture = R.drawable.user4,
             company = "@google working on @android",
             location = "Sydney, Australia"
         ),
         GithubUser(
             username = "tipsy",
             fullName = "David",
-            profilePicture = 1,
+            profilePicture = R.drawable.user5,
             company = "Working Group Two",
             location = "Trondheim, Norway"
         ),
         GithubUser(
             username = "ravi8x",
             fullName = "Ravi Tamada",
-            profilePicture = 1,
+            profilePicture = R.drawable.user6,
             company = "AndroidHive | Droid5",
             location = "India"
         ),
         GithubUser(
             username = "jasoet",
             fullName = "Deny Prasetyo",
-            profilePicture = 1,
+            profilePicture = R.drawable.user7,
             company = "@gojek-engineering",
             location = "Kotagede, Yogyakarta, Indonesia"
         ),
         GithubUser(
             username = "budioktaviyan",
             fullName = "Budi Oktaviyan",
-            profilePicture = 1,
+            profilePicture = R.drawable.user8,
             company = "@KotlinID",
             location = "Jakarta, Indonesia"
-        ),GithubUser(
+        ),
+        GithubUser(
             username = "hendisantika",
             fullName = "Hendi Santika",
-            profilePicture = 1,
+            profilePicture = R.drawable.user9,
             company = "@JVMDeveloperID @KotlinID @IDDevOps",
             location = "Bojongsoang - Bandung Jawa Barat"
+        ),
+        GithubUser(
+            username = "sidiqpermana",
+            fullName = "Sidiq Permana",
+            profilePicture = R.drawable.user10,
+            company = "Nusantara Beta Studio",
+            location = "Jakarta Indonesia"
         ),
 
     )
